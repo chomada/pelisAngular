f578373ae1de82fb54f8862099f83d0a82065d40
Federico Machado <fmachado@interbanking.com.ar>
2021-12-16 06:34:56 -0300

    comiteando

--- src/app/components/info/info.component.ts
+++ src/app/components/info/info.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from 'src/app/services/movie.service';
 import { Movie } from 'src/app/models/movie.model';
+import { CartService } from 'src/app/services/cart.service';
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
@@ -13,12 +14,13 @@ export class InfoComponent implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private movieService: MovieService
+    private movieService: MovieService,
+    private cartService: CartService
   ) { }
 
   ngOnInit(): void {
     this.movieService.getDetail(this.activatedRoute.snapshot.params['id'])
-    .subscribe(movie=>console.log(movie))
+    .subscribe(movie=>this.cartService.addMovie(movie as Movie))
   }
 
 }
--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,27 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { MovieService } from 'src/app/services/movie.service';
 import { Movie } from 'src/app/models/movie.model';
 import {Router} from '@angular/router';
+import { Subscription } from 'rxjs';
+//import { Subscription } from 'rxjs/Subscription';
+
+
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.scss']
 })
-export class MoviesComponent implements OnInit {
+export class MoviesComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  movies: Movie[]=[];
+  private subscription: Subscription | undefined;
 
   constructor(
     private movieService:MovieService,
-    private router:Router 
-    ) { }
-
-  movies: Movie[]=[]
+    private router:Router
+    ) {console.log("Hook Constructor"); }
 
   ngOnInit(): void {
-    this.movieService.getList().subscribe(movies =>this.movies=movies)
+    this.subscription= this.movieService.getList().subscribe(movies =>this.movies=movies);
+    console.log("Hook On Init")
   }
   navigateToDetail(id:number){
     this.router.navigate(['movies',id]);
 
+  }
+  ngAfterViewInit(): void {
+    console.log("Hook After View Init");
+  }
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+    console.log("Hook On Destroy");
   }
 
 }
